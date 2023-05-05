a04e6a62f7b10a5ad0d4a32b844473f70121978d
Bernardo C.D.A. Vasconcelos <35749099+bcdavasconcelos@users.noreply.github.com>
2023-05-04 21:01:57 -0300

    Update citefield.lua
    Fixed bug that caused crash when ref not found.

--- _extensions/citefield/citefield.lua
+++ _extensions/citefield/citefield.lua
@@ -90,7 +90,7 @@ function Pandoc (doc)
       function (r) return cite_id == r.id end
     ) -- end of get reference
     
-      if ref[the_arg] then -- if field is not empty
+      if ref and ref[the_arg] then -- if field is not empty
         local content = ref[the_arg] -- get field
         local title_field_emph = get_options(doc.meta).title_field_emph
         if the_arg == "author" or the_arg == "editor" or the_arg == "translator" then -- if field contains name
